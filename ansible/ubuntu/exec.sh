7044ad5523d408784e992eb102519c225d6fd08f
ker <ker10epere@gmail.com>
2024-04-10 18:33:38 +0800

    working

--- ansible/ubuntu/exec.sh
+++ ansible/ubuntu/exec.sh
@@ -7,8 +7,15 @@ ansible-vault create secret-123.yml
 # ENTER 123
 ansible ubuntu -i inventory -m ping --ask-vault-pass
 
+ansible webserver -m ping --ask-vault-pass
+
 # RUNNING run.yml PLAYBOOK
 # -K ASKS FOR SUDO PASSWORD
 ansible-playbook playbooks/run.yml -K --ask-vault-pass
 
 ansible-playbook playbooks/run.yml --ask-vault-pass -K
+
+ansible-playbook --list-tasks -v  run.yml 
+
+ansible-galaxy role remove geerlingguy.pip
+ansible-galaxy role remove geerlingguy.docker
