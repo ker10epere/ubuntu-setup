04e06177042526a8a9ac7963bde7deff39a173be
ker <ker10epere@gmail.com>
2024-04-10 20:45:45 +0800

    fix ubuntu ssh and ansible ubuntu

--- ansible/ubuntu/exec.sh
+++ ansible/ubuntu/exec.sh
@@ -5,17 +5,16 @@ ansible-vault create secret-123.yml
 
 # --ask-vault-pass ASKS FOR VAULT PASS FOR group_vars/all/secret-123.yml
 # ENTER 123
-ansible ubuntu -i inventory -m ping --ask-vault-pass
+ansible ubuntu -m ping --ask-vault-pass all
 
 ansible webserver -m ping --ask-vault-pass
 
 # RUNNING run.yml PLAYBOOK
 # -K ASKS FOR SUDO PASSWORD
-ansible-playbook playbooks/run.yml -K --ask-vault-pass
-
 ansible-playbook playbooks/run.yml --ask-vault-pass -K
 
-ansible-playbook --list-tasks -v  run.yml 
+
+ansible-playbook --list-tasks -v  playbooks/run.yml 
 
 ansible-galaxy role remove geerlingguy.pip
 ansible-galaxy role remove geerlingguy.docker
--- ubuntu/with-ssh/exec.sh
+++ ubuntu/with-ssh/exec.sh
@@ -6,6 +6,11 @@ sshpass -p priv ssh-copy-id -p 2221 -i ~/.ssh/id_ed25519.pub ubuntu@localhost
 
 ssh ubuntu@localhost -p 2221
 
+ssh root@localhost -p 2221
+ssh root@localhost -p 2222
+ssh root@localhost -p 2223
+ssh root@localhost -p 2224
+
 
 ssh-keygen -t ed25519 -C "ker10epere@gmail.com" -f .ssh/id_ed25519 -N ''
 
