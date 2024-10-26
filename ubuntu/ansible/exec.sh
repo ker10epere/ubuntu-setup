3a90bf21451cc5a4d7f95261b2461fcac2743e40
ker <ker10epere@gmail.com>
2024-10-26 18:44:06 +0800

    add ansible image

---
+++ ubuntu/ansible/exec.sh
@@ -0,0 +1,4 @@
+docker build --progress=plain -t ansible-ssh:0.0.1 -f ./manage-node/Dockerfile ./manage-node/
+docker buildx prune -af 
+# "-it" is important to be able to terminate spawned ssh
+docker run -it -p 9000:22 ansible-ssh:0.0.1
